d028c7ba37acec306852f77c88589643c110bd42
Sarmad <SarmadSattar1@gmail.com>
2015-02-04 15:45:05 +0500

    Toast Message

--- app/src/main/java/com/example/sarmad/sunshine/app/PlaceholderFragment.java
+++ app/src/main/java/com/example/sarmad/sunshine/app/PlaceholderFragment.java
@@ -11,8 +11,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -20,13 +27,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
     private String POST_CODE;
+    ArrayAdapter<String> adapter  ;
     public PlaceholderFragment() {
 
     }
@@ -37,11 +47,93 @@ public class PlaceholderFragment extends Fragment {
         setHasOptionsMenu(true);
     }
 
-    class FetchWeatherTask extends AsyncTask<String,Void,Void>{
+    class FetchWeatherTask extends AsyncTask<String,Void,String[]>{
+
+
+
+        private String getReadableDateString(long time){
+            // Because the API returns a unix timestamp (measured in seconds),
+            // it must be converted to milliseconds in order to be converted to valid date.
+            Date date = new Date(time * 1000);
+            SimpleDateFormat format = new SimpleDateFormat("E, MMM d");
+            return format.format(date).toString();
+        }
+
+
+
+
+        private String formatHighLows(double high, double low) {
+            // For presentation, assume the user doesn't care about tenths of a degree.
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+
+            private String[] getWeatherDataFromJson( String forecastJSonStr, int Days)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DATETIME = "dt";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJSonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            String[] resultStrs = new String[Days];
+            for(int i = 0; i < weatherArray.length(); i++) {
+                // For now, using the format "Day, description, hi/low"
+                String day;
+                String description;
+                String highAndLow;
+
+                // Get the JSON object representing the day
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                // The date/time is returned as a long.  We need to convert that
+                // into something human-readable, since most people won't read "1400356800" as
+                // "this saturday".
+                long dateTime = dayForecast.getLong(OWM_DATETIME);
+                day = getReadableDateString(dateTime);
+
+                // description is in a child array called "weather", which is 1 element long.
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                // Temperatures are in a child object called "temp".  Try not to name variables
+                // "temp" when working with temperature.  It confuses everybody.
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+
+            }
+                for (String result : resultStrs){
+
+                    Log.v(LOG_Tag,"preparing data" + result);
+                }
+
+
+
+            return resultStrs;
+        }
+
 
         public final String LOG_Tag=FetchWeatherTask.class.getSimpleName();
+
+
+
         @Override
-        protected Void doInBackground(String... params) {
+        protected String[] doInBackground(String... params) {
          // These two need to be declared outside the try/catch
 // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
@@ -53,7 +145,7 @@ public class PlaceholderFragment extends Fragment {
 
              String  format="json";
              String Unit="metric";
-             String  days="7";
+             int  days=7;
 
 
             try {
@@ -69,7 +161,7 @@ public class PlaceholderFragment extends Fragment {
                         .appendQueryParameter(Query_param,params[0])
                         .appendQueryParameter(Mode_param,format)
                         .appendQueryParameter(Unit_Param,Unit)
-                        .appendQueryParameter(DAYS_Param,days)
+                        .appendQueryParameter(DAYS_Param,String.valueOf(days))
                         .build();
                 Log.v(LOG_Tag,"URI Builder"+ForecastUriBuilder.toString());
 // Construct the URL for the OpenWeatherMap query
@@ -120,7 +212,52 @@ public class PlaceholderFragment extends Fragment {
                     }
                 }
             }
-            return null;
+
+            try {
+
+              return getWeatherDataFromJson(forecastJsonStr, days);
+
+            }
+
+            catch (JSONException e){
+
+                Log.e(LOG_Tag,e.getMessage(),e);
+                e.printStackTrace();
+            }
+
+            return  null;
+
+        }
+
+
+
+        /**
+         * <p>Runs on the UI thread after {@link #doInBackground}. The
+         * specified result is the value returned by {@link #doInBackground}.</p>
+         * <p/>
+         * <p>This method won't be invoked if the task was cancelled.</p>
+         *
+        // * @param strings The result of the operation computed by {@link #doInBackground}.
+         * @see #onPreExecute
+         * @see #doInBackground
+         * @see #onCancelled(Object)
+         */
+        @Override
+        protected void onPostExecute(String[] result) throws RuntimeException{
+            //super.onPostExecute(strings);
+
+            adapter.clear();
+          //  adapter.addAll(result);
+
+
+
+            for(String s : result){
+                Log.v(LOG_Tag," in on post execute" + s);
+                adapter.add(s);
+
+            }
+
+
         }
     }
     @Override
@@ -128,23 +265,44 @@ public class PlaceholderFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ArrayList<String> list = new ArrayList<String>();
+        list.add("dgdzsg");
+        list.add("sdgad");
+        //String[] s = new String[10];
 
-        list.add("Today - Sunny - 88/63");
-        list.add("Tomorow - Hot - 88/63");
-        list.add("Random - Dizzle - 88/63");
-        list.add("Today - Coll - 68/63");
-        list.add("Today - Coll - 68/63");
-        list.add("Today - Coll - 68/63");
-        list.add("Today - Coll - 68/63");
-        list.add("Today - Coll - 68/63");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+
+                adapter = new ArrayAdapter<String>(getActivity(),
                 R.layout.list_item_forecast,
                 R.id.list_item_forecast_textview,
-                list);
+                list
+                );
 
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            /**
+             * Callback method to be invoked when an item in this AdapterView has
+             * been clicked.
+             * <p/>
+             * Implementers can call getItemAtPosition(position) if they need
+             * to access the data associated with the selected item.
+             *
+             * @param parent   The AdapterView where the click happened.
+             * @param view     The view within the AdapterView that was clicked (this
+             *                 will be a view provided by the adapter)
+             * @param position The position of the view in the adapter.
+             * @param id       The row id of the item that was clicked.
+             */
+            @Override
+            public void onItemClick(AdapterView<?> adapterView , View view, int position, long id) {
+                String Result=adapter.getItem(position);
+                Toast.makeText(getActivity(),Result,position).show();
+
+
+            }
+        });
         listView.setAdapter(adapter);
+
+
 
         return rootView;
     }
@@ -162,7 +320,7 @@ public class PlaceholderFragment extends Fragment {
 
         if (id==R.id.action_refresh){
             FetchWeatherTask task = new FetchWeatherTask();
-            task.execute("94043");
+            task.execute("Karachi");
             return true;
         }
         else{
@@ -170,6 +328,11 @@ public class PlaceholderFragment extends Fragment {
             return super.onOptionsItemSelected(item);
         }
     }
+
+
+
+
+
 
 
 
